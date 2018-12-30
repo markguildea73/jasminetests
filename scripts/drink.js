9115998b2444592cf8a1cdff3ce2ddcabfc22e6b
Mark Guildea <mark.anthony.guildea@gmail.com>
2018-12-30 18:17:01 +0000

    created js script

---
+++ scripts/drink.js
@@ -0,0 +1,21 @@
+whatCanIDrink = function(age){
+    if (age < 0){
+        return "Sorry. I can’t tell what drink because that age is incorrect!";
+    }
+    if (age < 14){
+        return "Drink Toddy";
+    }
+    if (age< 18 && age>=14){
+        return "drink Coke";
+    }
+    if (age <21 && age >=18){
+        return "drink beer";
+    }
+    if (age < 130){
+        return "drink whiskey";
+    }
+    else {
+        return "sorry I cant tell what age you are"
+    }
+}
+console.log(whatCanIDrink(129))
